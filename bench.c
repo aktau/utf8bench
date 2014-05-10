22ce181f08d809ac217642e1077a785a6d7a2b92
Nicolas Hillegeer <nicolas@hillegeer.com>
2014-05-10 17:16:33 +0200

    sort results and format better

--- bench.c
+++ bench.c
@@ -1,6 +1,8 @@
 #include <sys/time.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
 
 int bjorn1CountCodePoints(const uint8_t* s, size_t* count);
 int bjorn2CountCodePoints(const uint8_t* s, size_t* count);
@@ -14,28 +16,47 @@ typedef int (cpcountfn)(const uint8_t *s, size_t *count);
 #define X(fn) \
     { fn, #fn }
 
+#define MAX_NAME_SIZE 256
+
+#define MODE_GITHUB 0x1
+
+struct result {
+    char file[MAX_NAME_SIZE];
+    char method[MAX_NAME_SIZE];
+    double runtime;
+    double bytes_per_codepoint;
+    double mb_per_sec;
+};
+
 void bench_fn(const char *name,
               int it,
               long fsize,
               const uint8_t *buf,
               const char *funname,
-              cpcountfn fn) {
+              cpcountfn fn,
+              struct result *res) {
     struct timeval t1, t2;
     double elapsedTime;
+
     gettimeofday(&t1, NULL);
     size_t cnt = 0xDEAD;
     for (int i = 0; i < it; ++i) {
         fn(buf, &cnt);
     }
-    gettimeofday(&t2, NULL); \
+    gettimeofday(&t2, NULL);
+
     elapsedTime = (t2.tv_sec - t1.tv_sec) * 1000.0;
     elapsedTime += (t2.tv_usec - t1.tv_usec) / 1000.0;
+
     double mb = (((double)fsize) / 1024.0 / 1024.0) * it;
     double sec = elapsedTime / 1000.0;
-    double mbsec = mb / sec;
     double bytes_codepoint_ratio = ((double)fsize) / (double)cnt;
-    printf("%-15s| %-25s| %6.1f ms | %15.1f | %3.1f\n",
-            name, funname, elapsedTime, bytes_codepoint_ratio, mbsec);
+
+    strncpy(res->file, name, sizeof(res->file));
+    strncpy(res->method, funname, sizeof(res->method));
+    res->runtime = elapsedTime;
+    res->bytes_per_codepoint = bytes_codepoint_ratio;
+    res->mb_per_sec = mb / sec;
 }
 
 char *readf(const char *fname, long *fsize) {
@@ -59,7 +80,37 @@ char *readf(const char *fname, long *fsize) {
     return buf;
 }
 
-int main() {
+int cmp_results(const void * a, const void * b) {
+  const struct result *r1 = a;
+  const struct result *r2 = b;
+
+  return r2->mb_per_sec - r1->mb_per_sec;
+}
+
+void print_results(const struct result *results, size_t num, uint32_t mode) {
+    printf("file           | function                 | runtime   | bytes/codepoint | mb/sec\n");
+    printf("---------------|--------------------------|-----------|-----------------|-------\n");
+    for (size_t i = 0; i < num; ++i) {
+        const struct result *res = &results[i];
+        if (mode & MODE_GITHUB && i == 0) {
+            printf("**%-15s**| **%-25s**| **%6.1f ms** | **%15.1f** | **%3.1f**\n",
+                res->file, res->method, res->runtime, res->bytes_per_codepoint, res->mb_per_sec);
+        }
+        else {
+            printf("%-15s| %-25s| %6.1f ms | %15.1f | %3.1f\n",
+                res->file, res->method, res->runtime, res->bytes_per_codepoint, res->mb_per_sec);
+        }
+    }
+}
+
+int main(int argc, char **argv) {
+    (void)argv;
+
+    uint32_t mode = 0;
+    if (argc > 1) {
+        mode |= MODE_GITHUB;
+    }
+
     struct {
         const char *name;
         int iterations;
@@ -81,18 +132,24 @@ int main() {
     };
 
     for (size_t f = 0; f < NELEM(files); ++f) {
-        printf("file           | function                 | runtime   | bytes/codepoint | mb/sec\n");
-        printf("---------------|--------------------------|-----------|-----------------|-------\n");
         long fsize = 0;
         char *buf = readf(files[f].name, &fsize);
         if (!buf) {
             continue;
         }
+
+        struct result results[NELEM(xfns)];
+
         for (size_t i = 0; i < NELEM(xfns); ++i) {
             bench_fn(files[f].name, files[f].iterations, fsize,
                 (uint8_t *) buf,
-                xfns[i].name, xfns[i].fn);
+                xfns[i].name, xfns[i].fn,
+                &results[i]);
         }
+
+        qsort(results, NELEM(results), sizeof(results[0]), cmp_results);
+        print_results(results, NELEM(results), mode);
+
         free(buf);
         printf("\n");
     }
